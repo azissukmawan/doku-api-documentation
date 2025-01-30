ef35ba01430462705ca75740795bc7aaf3fbe1a1
amawan <sukmawanabdul@gmail.com>
2025-01-30 14:31:28 +0700

    Add Swagger UI files

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,19 @@
+window.loadSpec = function (url) {
+  if (window.ui) {
+    // Destroy existing Swagger UI instance
+    window.ui.getConfigs().presetApis.forEach((api) => api.api.destroy());
+  }
+
+  // Initialize Swagger UI with the selected spec
+  window.ui = SwaggerUIBundle({
+    url: url,
+    dom_id: "#swagger-ui",
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    layout: "StandaloneLayout",
+  });
+};
+
+// Optional: Load a default spec on page load
+window.onload = function () {
+  loadSpec("../basic_and_full_request.json"); // Default to v1
+};
